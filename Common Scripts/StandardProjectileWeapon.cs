df98e112fafd573d00d6412c392b5849c1d5e328
Rus <72731965+DefinitelyRus@users.noreply.github.com>
2025-07-23 19:16:35 +0800

    Added `Projectiles` property

--- Common Scripts/StandardProjectileWeapon.cs
+++ Common Scripts/StandardProjectileWeapon.cs
@@ -1,7 +1,12 @@
 using Godot;
+using Godot.Collections;
 namespace CommonScripts;
 
 public partial class StandardProjectileWeapon : StandardWeapon
 {
+	#region Nodes & Components
 
+	public Dictionary<string, StandardProjectile> Projectiles { get; private set; }
+
+	#endregion
 }
