d0501bc6c1a82829431643082d0a99e0f70431f3
Rus <72731965+DefinitelyRus@users.noreply.github.com>
2025-07-26 06:41:36 +0800

    Update StandardProjectileWeapon.cs

--- Common Scripts/StandardProjectileWeapon.cs
+++ Common Scripts/StandardProjectileWeapon.cs
@@ -1,12 +1,46 @@
 using Godot;
-using Godot.Collections;
 namespace CommonScripts;
 
 public partial class StandardProjectileWeapon : StandardWeapon
 {
 	#region Nodes & Components
 
-	public Dictionary<string, StandardProjectile> Projectiles { get; private set; }
+	[ExportGroup("Nodes & Components")]
+	[Export] public PackedScene Projectile { get; private set; } = null!;
+
+	#endregion
+
+	#region Overrides
+
+	public override void Attack(bool v = false, int s = 0) {
+		Log.Me(() => "Spawning projectile...", v, s + 1);
+
+		if (Projectile == null) {
+			Log.Warn(() => "No projectile assigned. Cannot attack.", v, s + 1);
+			return;
+		}
+
+		StandardProjectile projectileInstance = Projectile.Instantiate<StandardProjectile>();
+		projectileInstance.GlobalPosition = AttackOrigin;
+		projectileInstance.RotationDegrees = AimDirection;
+		projectileInstance.WeaponOwner = WeaponOwner;
+	}
+
+	#endregion
+
+	#region Godot Callbacks
+
+	public override void _EnterTree() {
+		Log.Me(() => $"A StandardProjectileWeapon has entered the tree. Passing to StandardWeapon...", LogReady);
+
+		base._EnterTree();
+
+		Log.Me(() => "Checking properties...", LogReady);
+
+		if (Projectile == null) Log.Err(() => "No projectile assigned. Cannot attack.", LogReady);
+
+		Log.Me(() => $"Done checking properties for {ItemID}.", LogReady);
+	}
 
 	#endregion
 }
