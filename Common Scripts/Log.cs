8cd0e6aca6956fa45da28958309ae54dc4be7bb9
Rus <72731965+DefinitelyRus@users.noreply.github.com>
2025-07-26 01:11:48 +0800

    Added warning and error logging

--- Common Scripts/Log.cs
+++ Common Scripts/Log.cs
@@ -6,93 +6,226 @@ using System.Runtime.CompilerServices;
 using Godot;
 namespace CommonScripts;
 
-#nullable enable
-
 public partial class Log : Node {
-    #region Static logging
+	#region Static logging
 
-    /// <summary>
-    /// Logs a message to the console with additional context information.
-    /// </summary>
-    /// <param name="message">
-    /// The message to log.
-    /// </param>
-    /// <param name="useFilePath">
-    /// Whether to use the file path and line number for logging instead of the class and method names.
-    /// </param>
-    /// <param name="frame">
-    /// How many stack layers deep this function is called from.
-    /// Change this if you are calling this function from a helper function.
-    /// </param>
-    /// <param name="filePath">
-    /// This is auto-filled on compile time.
-    /// It's the entire folder structure that contains the file that called this funciton.
-    /// </param>
-    /// <param name="line">
-    /// This is auto-filled on compile time.
-    /// It's the line number from the source code file.
-    /// </param>
-    public static string Message(
-        string message,
-        bool printToConsole = true,
-        int stack = 0,
-        bool useFilePath = false,
-        int frame = 1,
-        [CallerFilePath] string filePath = "",
-        [CallerLineNumber] int line = 0
+    public enum PrintMode {
+        Message,
+        Warning,
+		Error,
+        FileOnly
+	}
+
+	/// <summary>
+	/// Logs a message to the console with additional context information.
+	/// </summary>
+	/// <param name="message">
+	/// The message to log.
+	/// </param>
+	/// <param name="printAs">
+	/// Whether to print the message as a regular message, warning, or error, or if the message should only be printed to file.
+	/// </param>
+	/// <param name="stack">
+	/// Basically how many spaces to indent the message.
+	/// Used for improving readaibility in verbose step-by-step logs.
+	/// </param>
+	/// <param name="useFilePath">
+	/// Whether to use the file path and line number for logging instead of the class and method names.
+	/// </param>
+	/// <param name="frame">
+	/// How many stack layers deep this function is called from.
+	/// Change this if you are calling this function from a helper function.
+	/// </param>
+	/// <param name="filePath">
+	/// This is auto-filled on compile time.
+	/// It's the entire folder structure that contains the file that called this funciton.
+	/// </param>
+	/// <param name="line">
+	/// This is auto-filled on compile time.
+	/// It's the line number from the source code file.
+	/// </param>
+	public static string Message(string message, PrintMode printAs = PrintMode.Message, int stack = 0, bool useFilePath = false, int frame = 1, [CallerFilePath] string filePath = "", [CallerLineNumber] int line = 0
     ) {
         string className, methodName, prefix;
         string padding = new(' ', stack);
 
         MethodBase? method = new StackTrace(frame, false).GetFrame(0)?.GetMethod();
 
-        //Attempt getting the class and method name from the stack trace.
+        // Attempt getting the class and method name from the stack trace.
         if (method != null && !useFilePath) {
             className = method.DeclaringType?.Name ?? string.Empty;
             methodName = method.Name;
             prefix = $"{padding}[{className}.{methodName}]";
         }
 
-        //Use the file path, method name, and line number.
+        // Use the file path, method name, and line number.
         else {
             string fileName = Path.GetFileName(filePath);
             prefix = $"{padding}[{fileName} @ line {line}]";
         }
 
-        string loggedMessage = $"{prefix} {message}";
-        if (printToConsole) GD.Print(loggedMessage); //TODO: Replace with a proper logging framework.
+		// Insert prefix based on print mode.
+		string insert = string.Empty;
+		if (printAs == PrintMode.Warning) insert = "WARN: ";
+        else if (printAs == PrintMode.Error) insert = "ERROR: ";
 
-        return loggedMessage;
-    }
+		string loggedMessage = $"{prefix} {insert}{message}";
 
-    /// <summary>
-    /// Logs a message to the console with additional context information.
-    /// </summary>
-    /// <param name="message">
-    /// The message to log.
-    /// </param>
-    /// <param name="useFilePath">
-    /// Whether to use the file path and line number for logging instead of the class and method names.
-    /// </param>
-    public static void Me(string message, bool enabled = true, int stack = 0, bool useFilePath = false) {
+        // Print as regular message.
+        if (printAs == PrintMode.Message) {
+            GD.Print(loggedMessage); //TODO: Replace with a proper logging framework.
+        }
+
+		// Print as warning and push to warning stack.
+		else if (printAs == PrintMode.Warning) {
+			GD.Print(loggedMessage);
+			GD.PushWarning(loggedMessage);
+        }
+
+		// Print as error and push to error stack.
+		else if (printAs == PrintMode.Error) {
+			GD.Print(loggedMessage);
+			GD.PushError(loggedMessage);
+        }
+
+		return loggedMessage;
+	}
+
+	/// <summary>
+	/// Logs a message to the console with additional context information.
+	/// </summary>
+	/// <param name="message">
+	/// The message to log.
+	/// </param>
+	/// <param name="enabled">
+	/// Whether to print the message at all.
+	/// If you want to log a message to file, use <see cref="Message"/> instead.
+	/// </param>
+	/// <param name="stack">
+	/// How deep this function is in the stack trace.
+	/// Basically, all this does is indent the message by this number of spaces.
+	/// </param>
+	/// <param name="useFilePath">
+	/// Whether to use the file path and line number for logging instead of the class and method names.
+	/// </param>
+	public static void Me(string message, bool enabled = true, int stack = 0, bool useFilePath = false) {
         if (!enabled) return;
-        Message(message, enabled, stack, useFilePath, 2);
+        if (enabled == false) return;
+		Message(message, PrintMode.Message, stack, useFilePath, 2);
     }
 
-    /// <summary>
-    /// Logs a message to the console with additional context information. <br/>
-    /// This is best used for logging messages that don't always need to be logged,
-    /// saving computation time when the message <c>enabled = false</c>.
-    /// </summary>
-    /// <param name="messageFactory"></param>
-    /// <param name="enabled"></param>
-    /// <param name="stack"></param>
-    /// <param name="useFilePath"></param>
-    public static void Me(Func<string> messageFactory, bool enabled = true, int stack = 0, bool useFilePath = false) {
+	/// <summary>
+	/// Logs a message to the console with additional context information. <br/>
+	/// This is best used for logging messages that don't always need to be logged,
+	/// saving computation time when the message <c>enabled = false</c>.
+	/// </summary>
+	/// <param name="messageFactory">
+	/// The message to log, as a function that returns a string.
+	/// </param>
+	/// <param name="enabled">
+	/// Whether to print the message at all.
+	/// If you want to log a message to file, use <see cref="Message"/> instead.
+	/// </param>
+	/// <param name="stack">
+	/// How deep this function is in the stack trace.
+	/// Basically, all this does is indent the message by this number of spaces.
+	/// </param>
+	/// <param name="useFilePath">
+	/// Whether to use the file path and line number for logging instead of the class and method names.
+	/// </param>
+	public static void Me(Func<string> messageFactory, bool enabled = true, int stack = 0, bool useFilePath = false) {
         if (!enabled) return;
-        Message(messageFactory(), enabled, stack, useFilePath, 2);
+        Message(messageFactory(), PrintMode.Message, stack, useFilePath, 2);
     }
 
-    #endregion
-}
+	/// <summary>
+	/// Logs a warning message to the console with additional context information.
+	/// </summary>
+	/// <param name="message">
+	/// The message to log.
+	/// </param>
+	/// <param name="enabled">
+	/// Whether to print the message at all.
+	/// If you want to log a message to file, use <see cref="Message"/> instead.
+	/// </param>
+	/// <param name="stack">
+	/// How deep this function is in the stack trace.
+	/// Basically, all this does is indent the message by this number of spaces.
+	/// </param>
+	/// <param name="useFilePath">
+	/// Whether to use the file path and line number for logging instead of the class and method names.
+	/// </param>
+	public static void Warn(string message, bool enabled = true, int stack = 0, bool useFilePath = false) {
+        if (!enabled) return;
+        Message(message, PrintMode.Warning, stack, useFilePath, 2);
+	}
+
+	/// <summary>
+	/// Logs a warning message to the console with additional context information.
+	/// </summary>
+	/// <param name="messageFactory">
+	/// The message to log, as a function that returns a string.
+	/// </param>
+	/// <param name="enabled">
+	/// Whether to print the message at all.
+	/// If you want to log a message to file, use <see cref="Message"/> instead.
+	/// </param>
+	/// <param name="stack">
+	/// How deep this function is in the stack trace.
+	/// Basically, all this does is indent the message by this number of spaces.
+	/// </param>
+	/// <param name="useFilePath">
+	/// Whether to use the file path and line number for logging instead of the class and method names.
+	/// </param>
+	public static void Warn(Func<string> messageFactory, bool enabled = true, int stack = 0, bool useFilePath = false) {
+		if (!enabled) return;
+		Message(messageFactory(), PrintMode.Warning, stack, useFilePath, 2);
+	}
+
+	/// <summary>
+	/// Logs an error message to the console with additional context information.
+	/// </summary>
+	/// <param name="message">
+	/// The message to log.
+	/// </param>
+	/// <param name="enabled">
+	/// Whether to print the message at all.
+	/// If you want to log a message to file, use <see cref="Message"/> instead.
+	/// </param>
+	/// <param name="stack">
+	/// How deep this function is in the stack trace.
+	/// Basically, all this does is indent the message by this number of spaces.
+	/// </param>
+	/// <param name="useFilePath">
+	/// Whether to use the file path and line number for logging instead of the class and method names.
+	/// </param>
+	public static void Error(string message, bool enabled = true, int stack = 0, bool useFilePath = false) {
+        if (!enabled) return;
+        Message(message, PrintMode.Error, stack, useFilePath, 2);
+    }
+
+	/// <summary>
+	/// Logs an error message to the console with additional context information.
+	/// </summary>
+	/// <param name="messageFactory">
+	/// The message to log, as a function that returns a string.
+	/// </param>
+	/// <param name="enabled">
+	/// Whether to print the message at all.
+	/// If you want to log a message to file, use <see cref="Message"/> instead.
+	/// </param>
+	/// <param name="stack">
+	/// How deep this function is in the stack trace.
+	/// Basically, all this does is indent the message by this number of spaces.
+	/// </param>
+	/// <param name="useFilePath">
+	/// Whether to use the file path and line number for logging instead of the class and method names.
+	/// </param>
+	public static void Error(Func<string> messageFactory, bool enabled = true, int stack = 0, bool useFilePath = false) {
+		if (!enabled) return;
+		Message(messageFactory(), PrintMode.Error, stack, useFilePath, 2);
+	}
+
+		#endregion
+	}
 
