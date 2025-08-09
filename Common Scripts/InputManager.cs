a873543aafbea2e0ac01fe654dab3cef591d5cad
Rus <72731965+DefinitelyRus@users.noreply.github.com>
2025-08-09 10:54:02 +0800

    Changed 4-space indents to tab spaces

--- Common Scripts/InputManager.cs
+++ Common Scripts/InputManager.cs
@@ -3,15 +3,15 @@ namespace CommonScripts;
 
 public partial class InputManager : Node2D {
 
-    public StandardCharacter Character = null!;
-    public ControlSurface Control = null!;
+	public StandardCharacter Character = null!;
+	public ControlSurface Control = null!;
 
 	#region Debugging
 
 	[ExportGroup("Debugging")]
-    [Export] protected bool LogReady = true;
-    [Export] protected bool LogProcess = false;
-    [Export] protected bool LogPhysics = false;
+	[Export] protected bool LogReady = true;
+	[Export] protected bool LogProcess = false;
+	[Export] protected bool LogPhysics = false;
 
 	#endregion
 
@@ -20,13 +20,13 @@ public partial class InputManager : Node2D {
 	#region Movement Inputs
 
 	[ExportGroup("Input Listeners")]
-    [ExportSubgroup("Movement Inputs")]
-    [Export] public string MoveUp = "move_up";
-    [Export] public string MoveDown = "move_down";
-    [Export] public string MoveLeft = "move_left";
-    [Export] public string MoveRight = "move_right";
+	[ExportSubgroup("Movement Inputs")]
+	[Export] public string MoveUp = "move_up";
+	[Export] public string MoveDown = "move_down";
+	[Export] public string MoveLeft = "move_left";
+	[Export] public string MoveRight = "move_right";
 
-    private MovementModes _movementMode = MovementModes.Static;
+	private MovementModes _movementMode = MovementModes.Static;
 	[Export] public MovementModes MovementMode {
 		get => _movementMode;
 		set {
@@ -41,7 +41,7 @@ public partial class InputManager : Node2D {
 		}
 	}
 
-    [Export] public Vector2 MoveOverride = new();
+	[Export] public Vector2 MoveOverride = new();
 
 	/// <summary>
 	/// The different ways a character can receive movement direction inputs. <br/><br/>
@@ -78,7 +78,7 @@ public partial class InputManager : Node2D {
 
 	// TODO: Allow gamepad inputs.
 	private void ReceiveMovementInputs(bool v = false, int s = 0) {
-        Log.Me(() => "Listening for movement inputs...", v, s + 1);
+		Log.Me(() => "Listening for movement inputs...", v, s + 1);
 
         if (!Control.EnableMovement) {
             Log.Me(() => "Movement inputs are disabled. Skipping input processing.", v, s + 1);
@@ -92,59 +92,59 @@ public partial class InputManager : Node2D {
 
             case MovementModes.Static:  break;
 
-            case MovementModes.Dedicated:
+			case MovementModes.Dedicated:
 				if (Input.IsActionPressed(MoveUp)) moveDirection += new Vector2(0, -1);
-                if (Input.IsActionPressed(MoveDown)) moveDirection += new Vector2(0, 1);
-                if (Input.IsActionPressed(MoveLeft)) moveDirection += new Vector2(-1, 0);
-                if (Input.IsActionPressed(MoveRight)) moveDirection += new Vector2(1, 0);
-                break;
+				if (Input.IsActionPressed(MoveDown)) moveDirection += new Vector2(0, 1);
+				if (Input.IsActionPressed(MoveLeft)) moveDirection += new Vector2(-1, 0);
+				if (Input.IsActionPressed(MoveRight)) moveDirection += new Vector2(1, 0);
+				break;
 
-            case MovementModes.Follow:
-                if (MoveOverride != Vector2.Zero) {
+			case MovementModes.Follow:
+				if (MoveOverride != Vector2.Zero) {
 					Log.Me(() => $"Following `MoveOverride`...", v, s + 1);
 					moveDirection = MoveOverride;
-                }
-                else {
+				}
+				else {
 					Log.Me(() => $"Following `Controls.FacingDirection`...", v, s + 1);
 					moveDirection = Control.FacingDirection;
 				}
 				break;
 
-            case MovementModes.AlwaysFollow:
-                moveDirection = Control.FacingDirection;
-                break;
-
-            case MovementModes.Vector:
-                moveDirection = MoveOverride;
+			case MovementModes.AlwaysFollow:
+				moveDirection = Control.FacingDirection;
 				break;
 
-            default:
-                Log.Err(() => "An invalid movement mode was used. Cannot update `Controls.MovementDirection`.", v, s + 1);
-                return;
-        }
+			case MovementModes.Vector:
+				moveDirection = MoveOverride;
+				break;
+
+			default:
+				Log.Err(() => "An invalid movement mode was used. Cannot update `Controls.MovementDirection`.", v, s + 1);
+				return;
+		}
 
 		Control.MovementDirection = moveDirection;
-        Control.MovementMultiplier = moveDirection.Length();
+		Control.MovementMultiplier = moveDirection.Length();
 
 		Log.Me(() => $"MovementDirection ({moveDirection.X:F2}, {moveDirection.Y:F2}) -> ({Control.MovementDirection.X}, {Control.MovementDirection.Y}) at {Control.MovementMultiplier:F2}x...", v, s + 1);
 
 		Log.Me(() => "Done!", v, s + 1);
-    }
+	}
 
 	#endregion
 
 	#region Facing Inputs
 
 	[ExportSubgroup("Facing Inputs")]
-    [Export] public string FaceUp = "face_up";
-    [Export] public string FaceDown = "face_down";
-    [Export] public string FaceLeft = "face_left";
-    [Export] public string FaceRight = "face_right";
+	[Export] public string FaceUp = "face_up";
+	[Export] public string FaceDown = "face_down";
+	[Export] public string FaceLeft = "face_left";
+	[Export] public string FaceRight = "face_right";
 
-    private FacingModes _facingMode = FacingModes.Static;
+	private FacingModes _facingMode = FacingModes.Static;
 	[Export] public FacingModes FacingMode {
-        get => _facingMode;
-        set {
+		get => _facingMode;
+		set {
 			bool moveFollows = _movementMode == MovementModes.Follow || _movementMode == MovementModes.AlwaysFollow;
 			bool faceFollows = _facingMode == FacingModes.Follow || _facingMode == FacingModes.AlwaysFollow;
 
@@ -156,7 +156,7 @@ public partial class InputManager : Node2D {
 		}
 	}
 
-    [Export] public Vector2 FaceOverride = Vector2.Zero;
+	[Export] public Vector2 FaceOverride = Vector2.Zero;
 
 	/// <summary>
 	/// The different ways a character can receive face direction inputs. <br/><br/>
@@ -204,41 +204,41 @@ public partial class InputManager : Node2D {
         switch (FacingMode) {
             case FacingModes.Static: break;
 
-            case FacingModes.Dedicated:
+			case FacingModes.Dedicated:
 				if (Input.IsActionPressed(FaceUp)) faceDirection += new Vector2(0, -1);
 				if (Input.IsActionPressed(FaceDown)) faceDirection += new Vector2(0, 1);
 				if (Input.IsActionPressed(FaceLeft)) faceDirection += new Vector2(-1, 0);
 				if (Input.IsActionPressed(FaceRight)) faceDirection += new Vector2(1, 0);
-                break;
-
-            case FacingModes.Follow:
-                if (FaceOverride != Vector2.Zero) {
-                    Log.Me(() => $"Following `FaceOverride`...", v, s + 1);
-                    faceDirection = FaceOverride;
-                }
-
-                else {
-                    Log.Me(() => $"Following `Controls.MovementDirection`...", v, s + 1);
-                    faceDirection = Control.MovementDirection;
-				}
-                break;
-
-            case FacingModes.AlwaysFollow:
-                faceDirection = Control.MovementDirection;
-                break;
-
-            case FacingModes.Vector:
-                faceDirection = FaceOverride;
 				break;
 
-            default:
-                Log.Err(() => "An invalid facing mode was used. Cannot update `Controls.FacingDirection`.", v, s + 1);
-                break;
+			case FacingModes.Follow:
+				if (FaceOverride != Vector2.Zero) {
+					Log.Me(() => $"Following `FaceOverride`...", v, s + 1);
+					faceDirection = FaceOverride;
+				}
+
+				else {
+					Log.Me(() => $"Following `Controls.MovementDirection`...", v, s + 1);
+					faceDirection = Control.MovementDirection;
+				}
+				break;
+
+			case FacingModes.AlwaysFollow:
+				faceDirection = Control.MovementDirection;
+				break;
+
+			case FacingModes.Vector:
+				faceDirection = FaceOverride;
+				break;
+
+			default:
+				Log.Err(() => "An invalid facing mode was used. Cannot update `Controls.FacingDirection`.", v, s + 1);
+				break;
 		}
 
-        Control.FacingDirection = faceDirection;
-        
-        Log.Me(() => $"FacingDirection ({faceDirection.X:F2}, {faceDirection.Y:F2}) -> ({Control.FacingDirection.X}, {Control.FacingDirection.Y})...", v, s + 1);
+		Control.FacingDirection = faceDirection;
+		
+		Log.Me(() => $"FacingDirection ({faceDirection.X:F2}, {faceDirection.Y:F2}) -> ({Control.FacingDirection.X}, {Control.FacingDirection.Y})...", v, s + 1);
 
 		Log.Me(() => "Done!", v, s + 1);
 	}
@@ -290,6 +290,6 @@ public partial class InputManager : Node2D {
 		Log.Me(() => "Done!", LogProcess);
 	}
 
-    #endregion
+	#endregion
 
 }
