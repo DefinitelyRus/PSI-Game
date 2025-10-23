5b9489c5eb28344ee068dff706376138b3ce48cc
Rus <72731965+DefinitelyRus@users.noreply.github.com>
2025-10-23 17:54:31 +0800

    Update AIAgentManager.cs

--- Common Scripts/AIAgentManager.cs
+++ Common Scripts/AIAgentManager.cs
@@ -2,16 +2,15 @@ using Godot;
 namespace CommonScripts;
 
 public partial class AIAgentManager : Node2D {
+	[Export] public StandardCharacter Character = null!;
 	[Export] public ControlSurface ControlSurface = null!;
 	[Export] public NavigationAgent2D NavAgent = null!;
-
-	[Export] public float MoveSpeed = 150f;
 
 	[Export] public bool LogReady = true;
 	[Export] public bool LogProcess = false;
 	[Export] public bool LogPhysics = false;
 
-	[Export] public Vector2 TargetPosition => NavAgent.TargetPosition;
+	public Vector2 TargetPosition => NavAgent.TargetPosition;
 
 	private bool _hasTarget = false;
 
@@ -30,7 +29,7 @@ public partial class AIAgentManager : Node2D {
 		Log.Me(() => "Done!", v, s + 1);
 	}
 
-	private void MoveTo(Vector2 target, bool v = false, int s = 0) {
+	private void MoveTo(bool v = false, int s = 0) {
 		if (!_hasTarget || NavAgent.IsNavigationFinished()) {
 			ControlSurface.MovementDirection = Vector2.Zero;
 			ControlSurface.MovementMultiplier = 0f;
@@ -45,33 +44,32 @@ public partial class AIAgentManager : Node2D {
 		ControlSurface.MovementDirection = dir;	// Normalized in setter.
 		ControlSurface.FacingDirection = dir;	// Normalized in setter.
 		ControlSurface.MovementMultiplier = 1f;
-		NavAgent.SetVelocity(dir * MoveSpeed);
+		NavAgent.SetVelocity(dir * Character.Speed);
 
 		Log.Me(() => "Done!", v, s + 1);
 	}
 
 	public override void _Ready() {
-		StandardCharacter character = GetParentOrNull<StandardCharacter>();
 
-		if (character == null) {
-			Log.Err(() => "AIAgentManager must be a child of a StandardCharacter.", LogReady);
+		if (Character == null) {
+			Log.Err(() => "StandardCharacter is not assigned. Cannot proceed.", LogReady);
 			return;
 		}
 
 		if (ControlSurface == null) {
-			Log.Err(() => $"ControlSurface is not assigned for {character.InstanceID}.", LogReady);
+			Log.Err(() => $"ControlSurface is not assigned for {Character.InstanceID}.", LogReady);
 			return;
 		}
 
 		if (NavAgent == null) {
-			Log.Err(() => $"NavigationAgent2D is not assigned for {character.InstanceID}.", LogReady);
+			Log.Err(() => $"NavigationAgent2D is not assigned for {Character.InstanceID}.", LogReady);
 			return;
 		}
 
-		Log.Me(() => $"AIAgentManager is ready for {character.InstanceID}.", LogReady);
+		Log.Me(() => $"AIAgentManager is ready for {Character.InstanceID}.", LogReady);
 	}
 
 	public override void _PhysicsProcess(double delta) {
-		MoveTo(TargetPosition, LogPhysics);
+		MoveTo(LogPhysics);
 	}
 }
