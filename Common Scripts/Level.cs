f0bcb6e47c93a7515d5e7ed52f9300ae7f1d7339
Rus <72731965+DefinitelyRus@users.noreply.github.com>
2025-10-30 17:39:00 +0800

    Created Level

---
+++ Common Scripts/Level.cs
@@ -0,0 +1,37 @@
+using System;
+using Godot;
+namespace CommonScripts;
+
+public partial class Level : Node
+{
+	[Export] public Node2D[] SpawnPoints = [];
+	private int CurrentSpawnIndex = 0;
+
+	public void SpawnUnit(StandardCharacter unit)
+	{
+		AddChild(unit);
+		
+		if (SpawnPoints.Length == 0)
+		{
+			Log.Err("No spawn points defined for this level. Canceling.");
+			return;
+		}
+
+		AttemptSpawn:
+		try {
+			unit.GlobalPosition = SpawnPoints[CurrentSpawnIndex].GlobalPosition;
+			CurrentSpawnIndex = (CurrentSpawnIndex + 1) % SpawnPoints.Length;
+		}
+
+		catch (IndexOutOfRangeException e) {
+			Log.Warn(() => $"There are not enough spawn points ({SpawnPoints.Length}) to spawn unit at index {CurrentSpawnIndex}. Resetting spawn index to 0 and retrying.", true, true);
+			CurrentSpawnIndex = 0;
+			goto AttemptSpawn;
+		}
+
+		catch (NullReferenceException e) {
+			Log.Err(() => $"Spawn point at index {CurrentSpawnIndex} is null. Canceling.", true, true);
+			return;
+		}
+	}
+}
