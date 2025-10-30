36aee6e606d964361382538ec30106098ac93b4c
Rus <72731965+DefinitelyRus@users.noreply.github.com>
2025-10-30 08:17:37 +0800

    Added EntityManager

---
+++ Scenes/Master/EntityManager.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using Godot;
+namespace CommonScripts;
+
+public partial class EntityManager : Node
+{
+    /// <summary>
+    /// Characters that have been spawned in on the game world.
+    /// </summary>
+    public static List<StandardCharacter> LiveCharacters { get; private set; } = [];
+
+
+    public static void AddCharacter(StandardCharacter character)
+    {
+        LiveCharacters.Add(character);
+    }
+
+
+    public static void RemoveCharacter(StandardCharacter character)
+    {
+        bool wasRemoved = LiveCharacters.Remove(character);
+        if (!wasRemoved) Log.Warn($"Character {character.InstanceID} not found in `LiveCharacters`. Cannot remove.");
+    }
+
+
+}
