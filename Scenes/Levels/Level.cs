f50f5acd0137ba266063db2716e599805352d77a
Rus <72731965+DefinitelyRus@users.noreply.github.com>
2025-11-05 18:36:14 +0800

    Changed spawn stuff

--- Scenes/Levels/Level.cs
+++ Scenes/Levels/Level.cs
@@ -2,22 +2,30 @@ using System;
 using Godot;
 namespace CommonScripts;
 
-public partial class Level : Node2D
-{
-	[Export] public Node2D[] SpawnPoints = [];
+public partial class Level : Node2D {
+
+	[Export] public Node2D SpawnParent = null!;
+	[Export] public Node2D[] CameraNodePaths = [];
 	private int CurrentSpawnIndex = 0;
 
 
 	public void SpawnUnit(StandardCharacter unit) {
-		AddChild(unit);
+		Node2D[] SpawnPoints = [.. SpawnParent.GetChildren().OfType<Node2D>()];
 
 		if (SpawnPoints.Length == 0) {
 			Log.Err("No spawn points defined for this level. Canceling.");
 			return;
 		}
 
-	AttemptSpawn:
+		foreach (Node2D sp in SpawnPoints) {
+			if (sp == null) {
+				Log.Warn(() => "One of the spawn points is null. Please check the level setup.", true, true);
+			}
+		}
+
+		AttemptSpawn:
 		try {
+			AddChild(unit);
 			unit.GlobalPosition = SpawnPoints[CurrentSpawnIndex].GlobalPosition;
 			CurrentSpawnIndex = (CurrentSpawnIndex + 1) % SpawnPoints.Length;
 		}
