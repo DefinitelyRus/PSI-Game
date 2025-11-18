df5200776932ea20bfe1a6ce4bf7a8d42570ea1f
Rus <72731965+DefinitelyRus@users.noreply.github.com>
2025-11-18 10:23:21 +0800

    Added UpgradeManager + ScanAndPickup

--- Prefabs/StandardCharacter/Units/UpgradeManager.cs
+++ Prefabs/StandardCharacter/Units/UpgradeManager.cs
@@ -1,9 +1,10 @@
 using System.Collections.Generic;
+using System.Linq;
 using CommonScripts;
 using Godot;
 namespace Game;
 
-public partial class UpgradeManager : Node2D {
+public partial class UpgradeManager : Node {
 
     #region Properties
 
@@ -18,6 +19,7 @@ public partial class UpgradeManager : Node2D {
     private double _cooldownTimer = 0d;
     public bool IsOnCooldown => _cooldownTimer > 0d;
 
+
     public int[] GetPoweredItems() {
         List<int> indices = [];
 
@@ -29,6 +31,7 @@ public partial class UpgradeManager : Node2D {
 
         return [.. indices];
     }
+
 
     public int GetPoweredItemCount() {
         int count = 0;
@@ -42,11 +45,13 @@ public partial class UpgradeManager : Node2D {
         return count;
     }
 
+
     public bool IsItemPowered(int index) {
         if (index < 0 || index >= Items.Count) return false;
 
         return Items[index].IsEquipped;
     }
+
 
     public void SetItemPower(int index, bool powered) {
         if (index < 0 || index >= Items.Count) return;
@@ -80,9 +85,51 @@ public partial class UpgradeManager : Node2D {
     #region Items
 
     [ExportGroup("Item Management")]
+    [Export] public float PickupRadius { get; private set; } = 16f;
+    [Export] public float ScanInterval { get; private set; } = 0.5f;
+    private double _scanTimer = 0d;
+    public bool IsScanReady => _scanTimer <= 0d;
     [Export] public int MaxSlots { get; private set; } = 5;
     public int CurrentMaxSlots { get; private set; } = 2;
     public List<StandardItem> Items { get; private set; } = [];
+
+
+    public void ScanAndPickup(double delta) {
+        if (!IsScanReady) return;
+        _scanTimer = ScanInterval;
+
+        foreach (PhysicsBody2D body in EntityManager.Entities) {
+
+            // Ignore out of range
+            float distance = Character.GlobalPosition.DistanceTo(body.GlobalPosition);
+            if (distance > PickupRadius) continue;
+
+            // Ignore self
+            if (body == Character) continue;
+
+            // Ignore own children
+            List<Node2D> children = [.. Character.GetChildren(true).OfType<Node2D>()];
+            if (children.Contains(body)) continue;
+
+            // Ignore non-items
+            if (body is not StandardItem item) continue;
+
+            // Ignore non-world items
+            if (item.EntityType != StandardItem.EntityTypes.World) continue;
+
+            Pickup(item);
+            break;
+        }
+    }
+
+
+    public void Pickup(StandardItem item) {
+        item.PickUp();
+
+        // Do not add if the item is single-use.
+        if (item.UseCount == 0) AddItem(item);
+    }
+
 
     public void AddItem(StandardItem item) {
         if (Items.Count >= CurrentMaxSlots) {
@@ -117,7 +164,25 @@ public partial class UpgradeManager : Node2D {
     [ExportGroup("Nodes & Components")]
     [Export] public StandardCharacter Character { get; set; } = null!;
 
+	#endregion
+
+	#region Godot Callbacks
+
+	public override void _Process(double delta) {
+        // Cooldown Timer
+        if (IsOnCooldown) {
+            _cooldownTimer -= delta;
+            if (_cooldownTimer < 0d) _cooldownTimer = 0d;
+        }
+
+        // Scan Timer
+        if (!IsScanReady) {
+            _scanTimer -= delta;
+            if (_scanTimer < 0d) _scanTimer = 0d;
+        }
+
+        ScanAndPickup(delta);
+    }
+
     #endregion
-
-
 }
