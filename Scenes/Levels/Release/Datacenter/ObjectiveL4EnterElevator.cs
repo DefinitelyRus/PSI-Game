0432cb68f385bb35d9995f109317ee4d71fcd58c
Rus <72731965+DefinitelyRus@users.noreply.github.com>
2025-11-25 18:07:08 +0800

    Added objectives, enemy spawns, adjusted regions

---
+++ Scenes/Levels/Release/Datacenter/ObjectiveL4EnterElevator.cs
@@ -0,0 +1,67 @@
+using CommonScripts;
+using Godot;
+namespace Game;
+
+public partial class ObjectiveL4EnterElevator : StandardPanel {
+    [Export] public float PromptDelay = 10f;
+    private float _promptTimer = 0f;
+
+    public override void Interact(StandardCharacter character) {
+        // Check if the charge has been planted
+        Variant? chargePlantedData = GameManager.GetGameData($"L4_ChargePlanted", null);
+        bool isChargePlanted = chargePlantedData != null && chargePlantedData!.Value.AsBool();
+        if (!isChargePlanted && _promptTimer <= 0f) {
+            Log.Me(() => $"{character.CharacterName} tried to access the elevator but the charge has not been planted.");
+            UIManager.SetBottomOverlayText("The charge needs to be planted first...", 5f);
+            _promptTimer = PromptDelay;
+            return;
+        }
+
+        // Check if the power virus has been installed
+        Variant? powerVirusData = GameManager.GetGameData($"L4_PowerVirusInstalled", null);
+        bool isPowerVirusInstalled = powerVirusData != null && powerVirusData!.Value.AsBool();
+        if (!isPowerVirusInstalled && _promptTimer <= 0f) {
+            Log.Me(() => $"{character.CharacterName} tried to access the elevator but the power virus has not been installed.");
+            UIManager.SetBottomOverlayText("We need to install the power virus.", 5f);
+            _promptTimer = PromptDelay;
+            return;
+        }
+
+        // Check if the heat management system has been sabotaged
+        Variant? heatSabotagedData = GameManager.GetGameData($"L4_HeatManagementSabotaged", null);
+        bool isHeatSabotaged = heatSabotagedData != null && heatSabotagedData!.Value.AsBool();
+        if (!isHeatSabotaged && _promptTimer <= 0f) {
+            Log.Me(() => $"{character.CharacterName} tried to access the elevator but the heat management system has not been sabotaged.");
+            UIManager.SetBottomOverlayText("The heat control is still online!", 5f);
+            _promptTimer = PromptDelay;
+            return;
+        }
+
+        // Check if all alive units are at the elevator
+        foreach (StandardCharacter unit in Commander.GetAllUnits()) {
+            if (!unit.IsAlive) continue;
+
+            bool isAtLocation = ScanForUnit(unit);
+            if (!isAtLocation) return;
+        }
+        
+        GameManager.SetGameData("L3_EnteredElevator", null, true);
+        Log.Me(() => $"All units have entered the elevator!");
+
+        // Disable panel after interaction
+        IsEnabled = false;
+        Activated = true;
+
+        SceneLoader.NextLevel(); // TODO: Win
+    }
+
+	public override void _Process(double delta) {
+        ScanForPlayer();
+        HighlightPanel(delta);
+
+        if (_promptTimer > 0f) {
+            _promptTimer -= (float) delta;
+        }
+    }
+
+}
--- Scenes/Levels/Release/Datacenter/ObjectivePlantCharge.cs
+++ Scenes/Levels/Release/Datacenter/ObjectivePlantCharge.cs
@@ -1,32 +1,66 @@
 using Godot;
 using CommonScripts;
+using System.Threading.Tasks;
 namespace Game;
 
 public partial class ObjectivePlantCharge : StandardPanel {
 
     [Export] public AudioStream? MusicOnPlant = null;
     [Export] public Node2D[] CameraNodes = [];
-    public override void Interact(StandardCharacter character) {
+    [Export] public NavigationRegion2D? Nav = null;
+
+    public override async void Interact(StandardCharacter character) {
         Variant? alreadyActivated = GameManager.GetGameData("L4_ChargePlanted", null);
         if (alreadyActivated != null) return;
 
-        Log.Me(() => $"{character.CharacterName} has planted the charge at the datacenter!");
+        CameraNodes[^1] = character;
 
-        GameManager.SetGameData("L4_ChargePlanted", null, true);
-
-        if (CameraNodes.Length > 0) CameraMan.SetCameraPath(CameraNodes);
-
-        if (MusicOnPlant != null) AudioManager.StreamAudio(MusicOnPlant);
-        if (ActivationSound != null) AudioManager.StreamAudio(ActivationSound);
+        if (CameraNodes.Length > 0) CameraMan.SetTarget(CameraNodes[^1]);
 
         // Disable panel after interaction
         IsEnabled = false;
         Activated = true;
+        InputManager.AllowOverride = false;
+        UIManager.SetHUDVisible(false);
+        AIDirector.AllowSpawning = true;
+        if (Nav != null) Nav.Enabled = true;
+
+        await ToSignal(GetTree().CreateTimer(1.5f), "timeout");
+
+        if (ActivationSound != null) AudioManager.StreamAudio(ActivationSound);
+
+        if (CameraNodes.Length > 0) CameraMan.SetCameraPath(CameraNodes);
+
+        UIManager.SetBottomOverlayText($"Install the power virus...", 2f);
+
+        await ToSignal(GetTree().CreateTimer(2.7f), "timeout");
+        UIManager.SetBottomOverlayText($"Disable the heat control...", 2f);
+
+        await ToSignal(GetTree().CreateTimer(3f), "timeout");
+        UIManager.SetBottomOverlayText($"And escape right before it detonates...", 3f);
+
+        await ToSignal(GetTree().CreateTimer(4.1f), "timeout");
+
+        UIManager.SetBottomOverlayText($"Good luck.", 3f);
+        Log.Me(() => $"{character.CharacterName} has planted the charge at the datacenter!");
+
+        if (MusicOnPlant != null) AudioManager.StreamAudio(MusicOnPlant);
+        GameManager.SetGameData("L4_ChargePlanted", null, true);
+        InputManager.AllowOverride = true;
     }
 
     public override void _Process(double delta) {
         ScanForPlayer();
         HighlightPanel(delta);
     }
+
+	public override void _Ready() {
+        AIDirector.AllowSpawning = false;
+
+        // Disable nav region until charge is planted
+        if (Nav != null) Nav.Enabled = false;
+
+		base._Ready();
+	}
 
 }
