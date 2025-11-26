4ad91e1857a15ba76bf36bf1cdffb87579490e4f
Rus <72731965+DefinitelyRus@users.noreply.github.com>
2025-11-26 23:01:17 +0800

    Adjust audio volume calculation in AudioController
    Updated the PlayAudio method to multiply the clip volume by SFXVolume and UniversalVolume from AudioManager. Also switched AudioClips initialization to use collection expression syntax.

--- Prefabs/StandardCharacter/AudioController.cs
+++ Prefabs/StandardCharacter/AudioController.cs
@@ -4,16 +4,19 @@ using Godot.Collections;
 namespace Game;
 
 public partial class AudioController : Node2D {
-    [Export] public Dictionary<string, AudioStream> AudioClips { get; set; } = new();
+    [Export] public Dictionary<string, AudioStream> AudioClips { get; set; } = [];
 
     public void PlayAudio(string clipName, float volume = 1.0f) {
         bool hasClip = AudioClips.TryGetValue(clipName, out AudioStream? value);
 
+        // NOTE: We use this instead of AudioManager.StreamAudio2D
+        // because, for some reason, it complains that it's not running
+        // on the main thread.
         if (hasClip) {
             AudioStreamPlayer2D audioPlayer = new() {
                 Stream = value,
                 Autoplay = false,
-                VolumeLinear = volume
+                VolumeLinear = volume * AudioManager.Instance.SFXVolume * AudioManager.Instance.UniversalVolume
             };
 
             AddChild(audioPlayer);
